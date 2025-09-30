8d02e9de1457a4ad9474a0e46bf010899444de4b
jotapmartins <martins_jp19@yahoo.com>
2025-09-30 09:59:28 -0300

    Primeiro deploy do portfólio

---
+++ portifolio.js
@@ -0,0 +1,3 @@
+function abrirCurriculo(){
+window.open("https://feiedu-my.sharepoint.com/:b:/g/personal/unifjomartins_fei_edu_br/EexQCZEQbsxBixyJL8QpZBkBQiZMGXG7HsEXkfNFUrZRfw?e=Qe1dah", "target: blank")
+}
